d5a9377ac950ac0fe290426a3495c88d7cac9c6c
Kellen Fujimoto <kellen@kellenfujimoto.com>
2014-07-24 00:03:24 -0700

    Added models, index view, started template

--- internships/models.py
+++ internships/models.py
@@ -1,10 +1,67 @@
-from django.contrib.auth.models import User, Group
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 from django.db import models
+
+def validate_future_date(value, pub_date):
+    from django.utils import timezone
+    if timezone.now() < value:
+        return True
+    else:
+        raise ValidationError(u"%s comes before %s! Please pick a due date that is after the publication date" % (value, timezone.now())
 
 class Applicant(models.Model):
     user = models.OneToOneField(User)
 
-    email = models.EmailField(unique = True)
-    groups = models.ManyToManyField(Group)
+    email = models.EmailField(unique = True, db_index = True)
 
+    def __unicode__(self):
+        return self.email
 
+class Business(models.Model):
+    user = models.OneToOneField(User)
+
+    email = models.EmailField(unique = True, db_index = True)
+
+    def __unicode__(self):
+        return self.email
+
+class Attribute(models.Model):
+    name = models.CharField(max_length = 50)
+    slug = models.SlugField()
+
+    def __unicode__(self):
+        return self.name
+
+class Posting(models.Model):
+    DRAFT, PENDING, PUBLISHED = 'dr', 'pe', 'pb'
+    POST_STATUS_CHOICES = (
+        (DRAFT, 'Draft'),
+        (PENDING, 'Pending Review'),
+        (PUBLISHED, 'Published'),
+    )
+
+    poster = models.ForeignKey(Business)
+    title = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField(auto_now_add = True, verbose_name = 'date published')
+    due_date = models.DateTimeField(verbose_name = 'due date', validators = [validate_future_date])
+    post_status = models.CharField(max_length = 3, choices = POST_STATUS_CHOICES, default = DRAFT)
+    slug = models.SlugField(unique = True)
+    attributes = models.ManyToManyField(Attribute)
+    body = models.TextField()
+
+    def __unicode__(self):
+        return self.title
+
+    @property
+    def pub_status(self):
+        from django.utils import timezone
+        if self.status = PUBLISHED:
+            if self.pub_date > timezone.now():
+                return 'Queued'
+            else:
+                return 'Public'
+        else:
+            return None
+
+    class Meta:
+        ordering = ['-pub_date']
--- internships/views.py
+++ internships/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from internships.models import Posting, Applicant, Business, Attribute
+from django.contrib.auth.models import User
+
+class PostingList(ListView):
+    context_object_name = 'postings_list'
+    template_namE
+
+    def get_queryset(self):
+        from django.utils import timezone
+        self.postings = Posting.objects.filter(post_status__eq = 'pb').exclude(pub_date__gt = timezone.now())
